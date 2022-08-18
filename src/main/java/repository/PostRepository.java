d8022fcb3478aa1ff2d6be902000d3fa8b17393c
Yurii Parkhomenko <parkhomenko.yurij@gmail.com>
2022-08-18 21:28:30 +0300

    initial commit

---
+++ src/main/java/repository/PostRepository.java
@@ -0,0 +1,35 @@
+package repository;
+
+import controller.PostController;
+import model.Post;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class PostRepository {
+
+    private final Map<Long, Post> posts = new ConcurrentHashMap<>();
+    private AtomicLong id = new AtomicLong(0);
+    public List<Post> all() {
+        if(posts.isEmpty()) return Collections.emptyList();
+        return new ArrayList<>(posts.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.of(posts.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            Long newId = id.incrementAndGet();
+            post = post.createNew(newId);
+        }
+        posts.put(post.getId(), post);
+        return post;
+    }
+
+    public void removeById(long id) {
+        posts.remove(id, posts.get(id));
+    }
+}
---
+++ src/main/java/servlet/MainServlet.java
@@ -0,0 +1,55 @@
+package servlet;
+
+import controller.PostController;
+import repository.PostRepository;
+import service.PostService;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.regex.Pattern;
+
+public class MainServlet extends HttpServlet {
+
+    private PostController controller;
+    private final static String API_PATH = "/api/posts";
+    private final static String API_PATH_WITH_NUMBERS = API_PATH + "/\\d+";
+
+    @Override
+    public void init() {
+        final var repository = new PostRepository();
+        final var service = new PostService(repository);
+        controller = new PostController(service);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        final var path = req.getRequestURI();
+        if (Pattern.matches(API_PATH_WITH_NUMBERS, path)) {
+            final var id = extractId(path);
+            controller.getById(id, resp);
+            return;
+        }
+        controller.all(resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        controller.save(req.getReader(), resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        final var path = req.getRequestURI();
+        if (Pattern.matches(API_PATH_WITH_NUMBERS, path)) {
+            final var id = extractId(path);
+            controller.removeById(id, resp);
+        }
+    }
+
+    private long extractId(String path) {
+        String NUMBER_PATTERN = "[^0-9]";
+        return Long.parseLong(path.replaceAll(NUMBER_PATTERN, ""));
+    }
+}
